17ce0b4203fb5400186550d44eff23fc42469f6e
Andy Meagher <ameagher311@gmail.com>
2015-02-25 16:06:09 -0600

    added gameover message

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,15 +1,48 @@
-'use strict'
+'use strict';
+var player1 = 'X';
+var player2 = 'O';
+var isPlayer1Turn = true;
+var turns = 0;
 
 var fb = new Firebase('https://tictacohno.firebaseio.com/');
-
-$('form').submit(function(event){
-	var name = $('.name').val();
-	var newPlayer = ({Game: 'Player1'});
-	fb.push(newPlayer);
-	event.preventDefault();
-})
 
 $('button').click(function(){
   $('.hidden').show();
   $('button').hide();
 });
+
+$('td').one('click', function(){
+  if (isPlayer1Turn === true){
+  	$(this).append(player1);
+    isPlayer1Turn = false;
+    turns += 1;
+    checkTurn();
+  }
+  else {
+  	$(this).append(player2)
+    isPlayer1Turn = true;
+    turns += 1;
+    checkTurn();
+  }
+});
+
+function checkTurn(){
+  if(turns === 9){
+    $('.message').append($('<h1>GAME OVER</h1>'))
+  }
+}
+
+
+// function render(b){
+// 	var $table = $('<table></table>');
+
+// 	b._forEach(function(row)
+// 	{
+// 	  var $tr = $('<tr></tr>');
+// 	  row.forEach(function(cell){
+//         $tr.append($('<td>'+ cell + '</td>'));
+//         $table.append($tr);
+// 	  });
+// 	 });
+// }
+
