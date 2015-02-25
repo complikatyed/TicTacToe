8cd62234cfb7628ecc0d261a795f3088ac025167
Katye Russell <katye.russell@gmail.com>
2015-02-25 15:32:21 -0600

    Adding flip turns functionality for one game.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var fb = new Firebase('https://xogame.firebaseio.com/');
+var isPlayer1Turn = true;
+
 
 /*$('form').submit(function(event){
 	var name = $('.name').val();
@@ -8,20 +10,37 @@ var fb = new Firebase('https://xogame.firebaseio.com/');
 	fb.push(newPlayer);
 	event.preventDefault();
 })*/
+$('form').submit(function(event){
+	var i = 1;
+	i += 1;
+	var newGame = ({Games: 'Game'+ i});
+	fb.push(newGame);
+	event.preventDefault();
+})
 
 $('button').click(function(){
   $('.hidden').show();
   $('button').hide();
 });
 
-$('form').submit(function(event){
-	var name = $('.name').val();
-	var newPlayer = ({Game: 'Player1'});
-	fb.push(newPlayer);
-	event.preventDefault();
-})
+/*$('td').on('click', function(event) {
+	var $target = $(event.target);
+	$target.append('x');
+});*/
 
-function placePlayer(){
+$('td').one('click', function() {
+  if (isPlayer1Turn) {
+    $(this).append("X");
+    isPlayer1Turn = false;
+  } else {
+    $(this).append("O");
+    isPlayer1Turn = true;
+  }
+});
+
+
+
+//function placePlayer(){
 	//query the firebase
 
 	// look for game with 1 player
@@ -31,4 +50,3 @@ function placePlayer(){
 	//if 1player game does NOT exist, create new game & insert player
 
 	//start game play
-}
