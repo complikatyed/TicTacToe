2d7892cd130b1d04e6d1c5ffa72c3787fff3fda1
Margaret Clinard <margaret.clinard@gmail.com>
2015-02-25 12:37:27 -0600

    start button shows table

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,12 @@
 'use strict'
 
 var fb = new Firebase('https://tictacohno.firebaseio.com/');
+
+$(document).ready(function () {
+
+})
+
+$('button').click(function(){
+  $('.hidden').show();
+  $('button').hide();
+});
