576ff362992ace3a778028f300694bc5406e54ee
Andy Meagher <ameagher311@gmail.com>
2015-02-25 23:11:08 -0600

    got winner with JSON stringify method

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,6 +3,10 @@ var player1 = 'X';
 var player2 = 'O';
 var isPlayer1Turn = true;
 var turns = 0;
+var xMoves = [];
+var oMoves = [];
+var possibleWins = ['0', '1', '2'];
+var winString = JSON.stringify(possibleWins);
 
 var fb = new Firebase('https://tictacohno.firebaseio.com/');
 
@@ -14,21 +18,33 @@ $('button').click(function(){
 $('td').one('click', function(){
   if (isPlayer1Turn === true){
   	$(this).append(player1);
+  	xMoves = xMoves.[$(this).attr('class')];
     isPlayer1Turn = false;
     turns += 1;
+    checkWinner();
     checkTurn();
   }
   else {
   	$(this).append(player2)
+  	oMoves = oMoves.concat($(this).attr('class'));
     isPlayer1Turn = true;
     turns += 1;
+    checkWinner();
     checkTurn();
   }
 });
 
+function checkWinner(){
+	if(JSON.stringify(xMoves) == winString){
+		$('.title').hide();
+		$('.message').append($('<h1>X WON THE GAME!!!!</h1>'));
+	}
+}
+
 function checkTurn(){
   if(turns === 9){
-    $('.message').append($('<h1>GAME OVER</h1>'))
+    $('.message').append($('<h1>GAME OVER</h1>'));
+    $('.title').hide();
   }
 }
 
