0daa9e31aa6345a0cc2c3849191edb6e3282821f
Katye Russell <katye.russell@gmail.com>
2015-02-26 10:04:58 -0600

    Incorporating Andy's code for game win and tie.  Changed a couple of variables for clarity.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,8 +2,8 @@
 var player1 = '<img src="/img/taco.jpg" height="100px" width="100px">';
 var player2 = '<img src="/img/burrito.jpg" height="100px" width="100px">';
 var isPlayer1Turn = true;
-var xWin = false;
-var oWin = false;
+var p1Win = false;
+var p2Win = false;
 var turns = 0;
 var a1;
 var a2;
@@ -16,24 +16,25 @@ var c2;
 var c3;
 
 
-
+// Monitors game play, switches between players & looks for win to end game
 $('td').one('click', function(){
-  if (isPlayer1Turn === true && xWin == false && oWin == false){
+  if (isPlayer1Turn === true && p1Win == false && p2Win == false){
   	$(this).append(player1);
     isPlayer1Turn = false;
     turns += 1;
     checkWinner();
-    checkTurn();
+    checkTie();
   }
-  else if(isPlayer1Turn == false && xWin == false && oWin == false) {
+  else if(isPlayer1Turn == false && p1Win == false && p2Win == false) {
   	$(this).append(player2)
     isPlayer1Turn = true;
     turns += 1;
     checkWinner();
-    checkTurn();
+    checkTie();
   }
 });
 
+// Converts table cell variables into jQuery objects
 function boardCheck() {
     a1 = $('.a1').html();
     a2 = $('.a2').html();
@@ -48,7 +49,7 @@ function boardCheck() {
 
 function checkWinner(){
 	boardCheck();
-	// CHECKS IF X WON
+	// Checks if player1 (Tacos) won
     if ((a1 == a2 && a1 == a3 && (a1 == player1)) || //first row
     (b1 == b2 && b1 == b3 && (b1 == player1)) || //second row
     (c1 == c2 && c1 == c3 && (c1 == player1)) || //third row
@@ -57,9 +58,10 @@ function checkWinner(){
     (a3 == b3 && a3 == c3 && (a3 == player1)) || //third column
     (a1 == b2 && a1 == c3 && (a1 == player1)) || //diagonal 1
     (a3 == b2 && a3 == c1 && (a3 == player1)) //diagonal 2
-    ){ xWin = true;
+    ){ p1Win = true;
        $('.message').append($('<h1>TACOS FOR THE WIN!</h1>'));
        $('.title').hide();  }
+    // Checks if player2 (Burritos) won
     else if ((a1 == a2 && a1 == a3 && (a1 == player2)) || //first row
     (b1 == b2 && b1 == b3 && (b1 == player2)) || //second row
     (c1 == c2 && c1 == c3 && (c1 == player2)) || //third row
@@ -68,20 +70,18 @@ function checkWinner(){
     (a3 == b3 && a3 == c3 && (a3 == player2)) || //third column
     (a1 == b2 && a1 == c3 && (a1 == player2)) || //diagonal 1
     (a3 == b2 && a3 == c1 && (a3 == player2)) //diagonal 2
-    ){ oWin = true;
+    ){ p2Win = true;
       $('.message').append($('<h1>BURRITOS FOR THE WIN!</h1>'));
       $('.title').hide();  }
   }
 
-
-
-function checkTurn(){
-  if(turns === 9 && xWin == false && owin == false){
+// Checks for a tie game and appends message
+function checkTie(){
+  if(turns === 9 && p1Win == false && p2Win == false){
     $('.message').append($('<h1>TIE GAME!</h1>'));
     $('.title').hide();
   }
 }
-
 
 // Makes game table appear on page
 $('.play').click(function(){
